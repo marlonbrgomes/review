9c12a9a5104bcd92fd2acf3f724bc7f099296634
Marlon Gomes <marlonbrgomes@gmail.com>
2024-05-15 17:26:04 -0300

    test number 3

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func helloWorld() {
 	fmt.Println("Hello, World!")
 }
 
 func main() {
-	helloWorld()
+	r := gin.Default()
+
+	r.GET("/hello-world", func(c *gin.Context) {
+		helloWorld()
+		c.String(200, "Hello, World!")
+	})
+
+	r.Run()
 }
